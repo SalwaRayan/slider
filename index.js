df9b2d143e3828e702ac71fffba52014cc62ad1f
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-20 17:27:57 +0200

    completed bonus 1

--- index.js
+++ index.js
@@ -20,12 +20,22 @@ $(function(){
       $('.slider').animate({
         left: `-${index * 100}%`
       }, 1000)
+    } else {
+      index = -1
+      $('.slider').animate({
+        left: `-${index * 100}%`
+      }, 1000)
     }
   })
 
   $(".left-arrow").click(function() {
     if (index > 0){
       index --
+      $('.slider').animate({
+        left: `-${index * 100}%`
+      }, 1000)
+    } else {
+      index = 4
       $('.slider').animate({
         left: `-${index * 100}%`
       }, 1000)
