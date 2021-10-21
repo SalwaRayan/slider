3f6bfc15d09d65f426a84472da188c303a0b5d12
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-21 10:25:12 +0200

    completed bonus 2

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ $(function(){
   var slider = $('.slider')
   var slides = slider.find('.slide')
   var index = 0
+  var timer = 2500
   
   $('.point').click(function() {
     index = $(this).index()
     $('.slider').animate({
       left: `-${index * 100}%`
     }, 1000)
+    clearInterval(interval)
+    setInterval(blipblop,5000)
   })
 
   $(".right-arrow").click(function() {
@@ -26,6 +29,8 @@ $(function(){
         left: `-${index * 100}%`
       }, 1000)
     }
+    clearInterval(interval)
+    setInterval(blipblop,5000)
   })
 
   $(".left-arrow").click(function() {
@@ -40,12 +45,32 @@ $(function(){
         left: `-${index * 100}%`
       }, 1000)
     }
+    clearInterval(interval)
+    setInterval(blipblop,5000)
   })
+
+  var interval = setInterval(blipblop,5000)
+
+  function blipblop (){
+      slider.animate({
+        left: `-${index * 100}%`
+      }, timer, function(){
+        index ++
+        
+        if (index > 4) {
+          index = 0
+          slider.animate({
+            left: `-${index * 100}%`
+          },5000)
+        }
+      })
+    }
+
 
   // setInterval(function(){
   //   slider.animate({
   //     left: `-=${+100}%`
-  //   }, 2500, function(){
+  //   }, timer, function(){
   //     currentSlide ++
   //     if (currentSlide === slides.length) {
   //       slider.css("left", 0)
@@ -54,13 +79,4 @@ $(function(){
   //   })
   // }, pause)
 
-
-
-
 })
-
-// var slides = $(".slide")
-// var dots = $(".point")
-
-// console.log(slides)
-// console.log(dots)
