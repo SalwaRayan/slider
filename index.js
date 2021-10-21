64fc8b4b6b9eb97f867cd43bab80629c433371cd
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-21 14:30:19 +0200

    completed bonus 2

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ $(function(){
   var index = 0
   var timer = 2500
   var interval = setInterval(blipblop, pause)
-  
+
+  $(".slide").each(function(){
+    $(this).css("background-image", `url("https://picsum.photos/1600/900?t=${Math.random()}")`)
+  })
+
+  function blipblop (){
+    slider.animate({
+      left: `-${index * 100}%`
+    }, timer, function(){
+      index ++
+      
+      if (index > 4) {
+        index = 0
+        slider.animate({
+          left: `-${index * 100}%`
+        }, pause)
+      }
+    })
+  }
+
   $('.point').click(function() {
     index = $(this).index()
+
     $('.slider').animate({
       left: `-${index * 100}%`
     }, 1000)
+
     clearInterval(interval)
     setInterval(blipblop, pause)
   })
@@ -18,15 +39,18 @@ $(function(){
   $(".right-arrow").click(function() {
     if (index < 4){
       index ++
+
       $('.slider').animate({
         left: `-${index * 100}%`
       }, 1000)
     } else {
       index = -1
+
       $('.slider').animate({
         left: `-${index * 100}%`
       }, 1000)
     }
+
     clearInterval(interval)
     setInterval(blipblop, pause)
   })
@@ -34,32 +58,20 @@ $(function(){
   $(".left-arrow").click(function() {
     if (index > 0){
       index --
+
       $('.slider').animate({
         left: `-${index * 100}%`
       }, 1000)
     } else {
       index = 4
+
       $('.slider').animate({
         left: `-${index * 100}%`
       }, 1000)
     }
+
     clearInterval(interval)
     setInterval(blipblop, pause)
   })
-
-  function blipblop (){
-      slider.animate({
-        left: `-${index * 100}%`
-      }, timer, function(){
-        index ++
-        
-        if (index > 4) {
-          index = 0
-          slider.animate({
-            left: `-${index * 100}%`
-          }, pause)
-        }
-      })
-    }
 
 })
